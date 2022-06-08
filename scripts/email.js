15fb31761425af8b0e0b70ea6ddd20408f21ce9d
mrsky1001 <nikita.nk16@yandex.ru>
2022-06-08 17:18:50 +0600

    fix

--- scripts/email.js
+++ scripts/email.js
@@ -1,1 +1,24 @@
+document.getElementById('contactsForm').addEventListener('submit', (e) => {
+    const url = "http://localhost:1234/api/email"
+    const request = new XMLHttpRequest()
 
+    request.open('POST', url, true)
+    request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+    request.onload = () => { // request successful
+        console.log(request.response)
+    }
+
+    request.onerror = (err) => {
+        console.log(err)
+    }
+
+    const obj = {}
+
+    for (var [key, value] of new FormData(e.target).entries()) {
+        obj[key] = value
+    }
+
+    request.send(JSON.stringify(obj)) // create FormData from form that triggered event
+    e.preventDefault()
+})
