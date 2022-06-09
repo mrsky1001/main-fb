375c3e556ea174eaaac777f519430c5378529c7d
mrsky1001 <nikita.nk16@yandex.ru>
2022-06-09 15:19:47 +0600

    fix

--- scripts/email.js
+++ scripts/email.js
@@ -1,24 +1,58 @@
-document.getElementById('contactsForm').addEventListener('submit', (e) => {
-    const url = "http://localhost:1234/api/email"
+
+const msgEl = document.getElementById('infoMsg')
+const msgContainerEl = document.getElementById('infoMsgContainer')
+
+const resetInfoOpacity = () => setTimeout(() => {
+    msgContainerEl.classList.add('info-msg-container__hide')
+
+    setTimeout(() => {
+        msgContainerEl.classList.remove('info-msg-container__show')
+    }, 500)
+}, 3000)
+
+function captchaSubmit(token) {
+    const url = "https://foma-blog:8081/api/email"
     const request = new XMLHttpRequest()
 
     request.open('POST', url, true)
     request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 
-    request.onload = () => { // request successful
-        console.log(request.response)
+    request.onload = () => {
+        const res = JSON.parse(request.response)
+        msgEl.innerText = res.message ? res.message : res
+
+        msgContainerEl.classList.remove('info-msg-container__hide')
+        msgContainerEl.classList.add('info-msg-container__show')
+
+        resetInfoOpacity()
     }
 
     request.onerror = (err) => {
-        console.log(err)
+        msgEl.innerText = err ?? 'Ошибка отправки заявки! Повторите попытку позже :)'
+        msgContainerEl.classList.add('info-msg-container__show')
+
+        resetInfoOpacity()
     }
 
-    const obj = {}
+    const obj = {responseKey: token}
 
-    for (var [key, value] of new FormData(e.target).entries()) {
+    for (var [key, value] of new FormData(document.getElementById('contactsForm')).entries()) {
         obj[key] = value
     }
 
     request.send(JSON.stringify(obj)) // create FormData from form that triggered event
+}
+
+
+document.getElementById('contactsForm').addEventListener('submit', (e) => {
+    grecaptcha.execute().then().catch(err => {
+        msgEl.innerText = err ?? 'Ошибка отправки заявки! Повторите попытку позже :)'
+        msgContainerEl.classList.add('info-msg-container__show')
+
+        resetInfoOpacity()
+    })
+
     e.preventDefault()
 })
+
+
